55b0e4e1a4429e13e6b36176c8ec869b8b2c23af
Joseph Samuel <samueljoseph211@gmail.com>
2026-05-02 17:23:47 -0400

    assignment complete: added about.ejs and footer template

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -10,9 +10,19 @@ indexRouter.get("/", (req, res) => {
       { href: "/", text: "Home" },
       { href: "about", text: "About" },
     ],
+    footer: "This is a footer",
   });
 });
-indexRouter.get("/about", (req, res) => res.send("You are at about.html"));
+indexRouter.get("/about", (req, res) => {
+  res.render("about", {
+    message: "You are at about.html",
+    footer: "This is a footer",
+    links: [
+      { href: "/", text: "Home" },
+      { href: "about", text: "About" },
+    ],
+  });
+});
 indexRouter.get("/contact", (req, res) => res.send("You are at contact.html"));
 indexRouter.post("/contact", (req, res) => res.send("You have successfully post to contact.html"));
 
